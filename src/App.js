96d2b9d868c41fdb6a452babf06bb75ebe0a45af
Blake Davis <blavis21@gmail.com>
2019-08-01 22:37:19 -0500

    Resume Page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title="Title" scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My Portfolio</Link>} scroll>
             <Navigation>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/contact">Contact</Link>
@@ -16,7 +16,7 @@ function App() {
                 <Link to="resume">Resume</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My Portfolio</Link>}>
             <Navigation>
                 <Link to="aboutme">About Me</Link>
                 <Link to="contact">Contact</Link>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,83 @@
 import React, { Component } from 'react'
+import { Grid, Cell } from 'react-mdl'
+import Education from './Education'
+import Experience from './Experience'
+import Skills from './Skills'
 
 export default class Resume extends Component {
     render() {
         return (
             <div>
-                <h1>Resume Page</h1>
+                <Grid>
+
+                    <Cell col={4}>
+                        <div style={{ textAlign: 'center' }}>
+                            <img
+                                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                                alt="avatar"
+                                style={{ height: '200px' }} />
+                        </div>
+
+                        <h2 style={{ paddingTop: '2em' }}>Blake Davis</h2>
+                        <h4 style={{ color: 'grey' }}>Programmer</h4>
+                        <hr style={{ borderTop: '3px solid #ffa17f', width: '50%' }} />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <hr style={{ borderTop: '3px solid #ffa17f', width: '50%' }} />
+                        <h5>Address</h5>
+                        <p>120 Clarice Court, White House, TN 37188</p>
+                        <h5>Phone</h5>
+                        <p>(615) 478-3592</p>
+                        <h5>Email</h5>
+                        <p>davis.blake21@gmail.com</p>
+                        <hr style={{ borderTop: '3px solid #ffa17f', width: '50%' }} />
+
+                    </Cell>
+
+                    <Cell className="resume-right-col" col={8}>
+                        <h2>Education</h2>
+                        <Education
+                            startYear={2003}
+                            endYear={2007}
+                            schoolName="Hendersonville High School"
+                            schoolDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+                        <Education
+                            startYear={2019}
+                            endYear={2019}
+                            schoolName="Nashville Software School"
+                            schoolDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+                        <hr style={{ borderTop: '3px solid #ffa17f' }} />
+                        <h2>Experience</h2>
+                        <Experience
+                            startYear={2008}
+                            endYear={1234}
+                            jobName="Job 1"
+                            jobDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+                        <Experience
+                            startYear={3423}
+                            endYear={1234}
+                            jobName="Job 2"
+                            jobDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+                        <Experience
+                            startYear={1234}
+                            endYear={1234}
+                            jobName="Job 3"
+                            jobDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+                        <hr style={{ borderTop: '3px solid #ffa17f' }} />
+                        <h2>Skills</h2>
+                        
+                        <Skills
+                            skill="HTML/CSS"
+                            progess={100} />
+                        <Skills
+                            skill="JavaScript"
+                            progess={75} />
+                        <Skills
+                            skill="HTML/CSS"
+                            progess={85} />
+
+                    </Cell>
+
+                </Grid>
             </div>
         )
     }
