6e095a538e09c91d344ad65f329d9dc11565d731
Blake Davis <blavis21@gmail.com>
2019-08-01 21:35:09 -0500

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react'
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
 
 export default class Contact extends Component {
+
     render() {
         return (
-            <div>
-                <h1>Contact</h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+
+                    <Cell col={6}>
+                        <h2>Blake Davis</h2>
+                        <img
+                            src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt="avatar"
+                            style={{ height: '250px' }} />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Cell>
+
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+
+                        <div className="contact-list">
+                            <List>
+
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-phone-square" aria-hidden="true" />
+                                        (615) 478-3592
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-envelope" aria-hidden="true" />
+                                        davis.blake21@gmail.com
+                                    </ListItemContent>
+                                </ListItem>
+                                
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-skype" aria-hidden="true" />
+                                        skype id
+                                    </ListItemContent>
+                                </ListItem>
+
+                            </List>
+                        </div>
+
+
+                    </Cell>
+                </Grid>
             </div>
         )
     }
