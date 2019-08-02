f88b8cfc7f86a8a5d17cc1e7d76b498d49712ab1
Blake Davis <blavis21@gmail.com>
2019-08-01 20:19:27 -0500

    created and styled landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import { Grid, Cell } from 'react-mdl'
 
 export default class LandingPage extends Component {
     render() {
         return (
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{ width: '100%', margin: 'auto' }}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
+                            alt="avatar"
+                            className="avatar-img" />
+
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr />
+
+                            <p>HTML/CSS | Bootstrap | JavaScript | React</p>
+
+                            <div className="social-links">
+
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/blavis21/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+
+                                {/* Github */}
+                                <a href="https://github.com/blavis21" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
